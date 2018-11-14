bf5fb2f7c5d738107816d4e7b8447e758cbb7f6b
Pitchoun <pierrecaprassesjb@gmail.com>
2018-11-14 13:57:20 +0100

    new doc

--- utils_cv/load_and_display_imagep.py
+++ utils_cv/load_and_display_imagep.py
@@ -1,17 +1,19 @@
 
 import cv2
 def load_and_display_image(filename):
-    img = cv2.imread(filename,0)
-    #taille = shape(img)
-    #for i in range(taille[0]):
-    #    for j in range(taille[1]):
-    #        px =img[i,j]
-    #        for i in range(2):
-    #            moyenne = (px[0]+px[1]+px[2])/3
-    #            px = [moyenne,moyenne,moyenne]
+    img = cv2.imread(filename)
+    taille = img.shape[:2]
+    px=[]
+    for i in range(taille[0]):
+        for j in range(taille[1]):
+             px =img[i,j]
+             moyenne = (px[0]+px[1]+px[2])/3
+             img[i,j] = [moyenne,moyenne,moyenne]
+    print(px)
     cv2.imshow('image',img)
     cv2.waitKey(0)
     #return
+
 
 
 
