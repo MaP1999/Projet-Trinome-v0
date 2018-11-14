1aaca8c5c01930d074b6a5c35a8978e06ab31124
Marvin <marvin.poiron@student-cs.fr>
2018-11-14 14:09:13 +0100

    j espere que ca va marcher

--- utils_cv/Fich1.py
+++ utils_cv/Fich1.py
@@ -1,2 +1,17 @@
+import cv2
+import numpy as np
+
+chdir='C://Users//Marvin//Documents//CentraleSupélec//Cours//CodingWeeks//Projet-Trinome-v0//data//tetris_blocks.png'
+tetris='tetris_blocks.png'
+
 def load_and_display_image(filename):
-    img=
+    img=cv2.imread(filename,0)
+    cv2.imshow('fenetre',img)
+    cv2.waitKey(0)
+    # print(img)
+
+
+
+
+
+
